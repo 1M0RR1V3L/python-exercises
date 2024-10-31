f5ed722c9c642c49040947cd61ba9944c13c03a0
1m0rr1v3l <lsxyz2017@gmail.com>
2024-10-31 15:07:52 -0300

    add some exercises

---
+++ all-exercises.py
@@ -0,0 +1,137 @@
+# 1) Crie uma variável chamada número e em seguida atribua a ela o valor 10. Em seguida, imprima o valor dessa variável.
+
+# numero = 10
+# print(numero)
+
+# 2)
+
+# frutas = ["maçã", "banana", "cereja"]
+
+# 3)
+
+# frutas = ["maçã", "banana", "cereja"]
+# print(frutas[0])
+
+# 4)
+
+# frutas = ["maçã", "banana", "cereja"]
+#
+# frutas[1] = "laranja"
+# print(frutas)
+
+
+# 5)
+
+# frutas = ["maçã", "banana", "cereja"]
+# frutas += ["uva"]
+#
+# print(frutas)
+
+# 6)
+#
+# frutas = ["maçã", "banana", "cereja"]
+#
+# frutas.remove("banana")
+#
+# print(frutas)
+
+# 7)
+
+# frutas = ["maçã", "banana", "cereja",]
+#
+# print(len(frutas))
+
+# 8)
+#
+# frutas = ["maçã", "banana", "cereja"]
+#
+# print(frutas[1], frutas[2])
+
+# 9)
+
+# frutas = ["maçã", "banana", "cereja"]
+#
+# i = 0
+# for i in range(len(frutas)):
+#     print(frutas[i])
+
+# 10)
+
+# frutas = ["maçã", "banana", "cereja"]
+#
+# if "maçã" in frutas:
+#     print(True)
+# else:
+#     print(False)
+
+# 11)
+
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+# list3 = [7, 8, 9]
+#
+# matriz = [list1, list2, list3]
+#
+# print(matriz)
+
+
+# 12) incompleta
+
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+# list3 = [7, 8, 9]
+#
+# matriz = [list1, list2, list3]
+#
+# print(matriz)
+
+# 13)
+
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+# list3 = [7, 8, 9]
+#
+# matriz = [list1, list2, list3]
+#
+# list4 = [10, 11, 12]
+#
+# matriz += [list4]
+#
+# print(matriz)
+
+
+# 14)
+
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#
+# quadrados = [x**2 for x in numeros ]
+# print(quadrados)
+
+
+# 15)
+
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# quadrados_par = [x**2 for x in numeros if x % 2 == 0 ]
+# print(quadrados_par)
+
+# 16)
+
+# numeros = [3, 6, 1, 8, 2, 5]
+#
+# numero_cres = sorted(numeros)
+# print(numero_cres)
+#
+# numero_decres = sorted(numeros, reverse=True)
+#
+# print(numero_decres)
+
+# 17)
+
+list_original = ["algo"]
+copia = list_original
+
+copia2 =list_original.copy()
+
+print(copia)
+print(copia2)
